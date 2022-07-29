a0b8a54bdd5d322662d1cf508874312407160385
LuizFelipe406 <luizfpereira406@gmail.com>
2022-07-29 08:27:58 -0300

    feat: setup inicial

---
+++ src/pages/Contact.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Contact() {
+  return(
+    <div>Eu sou a Contact page</div>
+  )
+}
+
+export default Contact;
---
+++ src/pages/Projects.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Projects() {
+  return(
+    <div>Eu sou a Projects page</div>
+  )
+}
+
+export default Projects;

ae5d123dd91913c7ddc2d42a08e3e058599fbc19
LuizFelipe406 <luizfpereira406@gmail.com>
2022-08-01 09:48:56 -0300

    feat: home page concluida

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import linkedin from '../images/linkedin-logo.png';
+import github from '../images/github-icon-2.png';
 import '../style/Header.css';
 
 function Header() {
   const { pathname } = useLocation();
   return(
     <header className="header-container">
-      <div className="title-container ms-5">
-        <h1 className="fs-5 my-0">Luiz Felipe Pereira</h1>
-        <h2 className="fs-6 my-0">Front-End Dev</h2>
+      <div className="header-content-container">
+        <div className="title-container ms-5">
+          <h1 className="fs-5 my-0">Luiz Felipe Pereira</h1>
+          <h2 className="fs-6 my-0">Front-End Dev</h2>
+        </div>
+        <div className="ms-4">
+          <a href="https://www.linkedin.com/in/luizfpereira406/" target="_blank" rel="noreferrer">
+            <img src={ linkedin } alt='linkedin icon' width="43px"/>
+          </a>
+          <a href="https://github.com/LuizFelipe406" target="_blank" rel="noreferrer">
+            <img src={ github } alt='github icon' width="33px"/>
+          </a>
+        </div>
       </div>
       <nav className="me-5 nav-container">
         <div className={ pathname === '/' ? "me-5 link-container" : "me-5"}>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import '../style/Home.css';
 
@@ -6,7 +7,21 @@ function Home() {
   return(
     <main className="main-home-page">
       <Header />
-      <span>Eu sou a home page</span>
+      <section className="ms-5 ps-5 mt-5 pt-5">
+        <div className="ms-5 ps-5 mt-5 pt-4">
+          <h1
+          className="home-title"
+          >
+            Welcome to my<br />Portfólio
+          </h1>
+          <p className=" home-text mt-5">
+            Olá me chamo Luiz Felipe Pereira, sou apaixonado por Tecnologia e como ela pode impactar a vida e o trabalho das pessoas ao redor do mundo, me considero uma pessoa inquieta, ambiciosa e com sede de aprendizado!, atualmente sou Desenvolvedor Front-End e estudando Desenvolvimento Web Full Stack na Trybe
+          </p>
+          <Link to="/projects">
+            <button className="home-button py-2 px-4 mt-5">see my projects!</button>
+          </Link>
+        </div>
+      </section>
     </main>
   )
 }
